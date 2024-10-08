1cb2f4d6e118b04bceb586751a286c8da310a17a
heroisuseless <35455575+HeroIsUseless@users.noreply.github.com>
2024-10-08 14:05:55 +0800

    fix: kk

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,5 +5,12 @@ module.exports = {
     'plugin:react/jsx-runtime',
     '@electron-toolkit/eslint-config-ts/recommended',
     '@electron-toolkit/eslint-config-prettier'
-  ]
+  ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react/no-unknown-property': 'off'
+  }
 }
