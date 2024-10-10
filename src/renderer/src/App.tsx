3db90a69e95d813318ff746e6067620afa16fe08
heroisuseless <35455575+HeroIsUseless@users.noreply.github.com>
2024-10-10 14:25:57 +0800

    fix: list

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -2,11 +2,13 @@ import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import { useState } from 'react'
 import ConfigEditor from '@/components/view/editor'
+import ParallaxList from './components/view/parallax-list'
 
 function App(): JSX.Element {
   return (
     <div className="fixed w-screen h-screen overflow-hidden">
-      <ConfigEditor />
+      {/* <ConfigEditor /> */}
+      <ParallaxList />
     </div>
   )
 }
